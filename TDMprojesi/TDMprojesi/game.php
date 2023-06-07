2694649a5c79e532edfd5bed8f5dd76fdf5795e5
InterML01 <84276862+InterML01@users.noreply.github.com>
2023-06-07 06:21:34 +0300

    Add files via upload

---
+++ TDMprojesi/TDMprojesi/game.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header('location: login.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>TDM PROJECT</title>
+  <link rel="stylesheet" type="text/css" href="css/styleG.css">
+  <style>
+    .container {
+      position: relative;
+    }
+
+    .go-back-btn {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      background-color: #223243;
+      color: white;
+      padding: 10px 20px;
+      border: none;
+      cursor: pointer;
+      font-size: 16px;
+      border-radius: 40%;
+    }
+
+    .go-back-btn:hover {
+      background-color: #243243;
+    }
+
+    .avatar {
+      position: absolute;
+      top: 10px;
+      right: 60px;
+      display: flex;
+      align-items: center;
+      font-size: 26px;
+      color: #fff;
+    }
+/* 
+    .avatar img {
+      width: 70px;
+      height: 70px;
+      border-radius: 50%;
+      margin-right: 5px;
+    } */
+
+    /* Rest of your existing CSS styles */
+  </style>
+  <script>
+    function goBack() {
+      window.history.back();
+    }
+
+    document.addEventListener('DOMContentLoaded', (event) => {
+      const audioSources = [
+        'https://www.fesliyanstudios.com/play-mp3/5248',
+        'https://www.fesliyanstudios.com/play-mp3/5262',
+        'https://www.fesliyanstudios.com/play-mp3/5265'
+      ];
+
+      const cards = document.querySelectorAll(".card");
+      cards.forEach((card, index) => {
+        card.addEventListener("mouseenter", () => {
+          var audio = new Audio(audioSources[index]);
+          audio.volume = 0.5; // Adjust the volume (0.0 to 1.0)
+          audio.currentTime = 0;
+          audio.play();
+        });
+      });
+    });
+  </script>
+</head>
+<body>
+  <div class="container">
+    <?php if (isset($_SESSION['username'])) : ?>
+      <div class="avatar">
+        <!-- <img src="<?php echo $randomLogoUrl; ?>" alt="User Avatar"> -->
+        <h3 style="color: #fff;">Player : <strong><?php echo $_SESSION['username']; ?></strong></h3>
+      </div>
+    <?php endif ?>
+
+    <div class="card">
+      <div class="lines"></div>
+      <div class="imgBx">
+        <img src="img/game1.png">
+      </div>
+      <div class="content">
+        <div class="details">
+          <h2>GAME 1</h2>
+          <p>Halat Oyunu</p>
+          <a href="oyunlar/halat_oyun2/index.html">PLAY</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="lines"></div>
+      <div class="imgBx">
+        <img src="img/game2.png">
+      </div>
+      <div class="content">
+        <div class="details">
+          <h2>GAME 2</h2>
+          <p>Fidan Oyunu</p>
+          <a href="#">PLAY</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="lines"></div>
+      <div class="imgBx">
+        <img src="img/game3.png">
+      </div>
+      <div class="content">
+        <div class="details">
+          <h2>GAME 3</h2>
+          <p>INFORMATION ABOUT GAME 3</p>
+          <a href="oyunlar/math_runner_webGL copy/index.html">PLAY</a>
+        </div>
+      </div>
+    </div>
+ิ<br>
+<br>
+   
+      <button class="go-back-btn" onclick="goBack()"> <strong>Back</strong></button>
+    
+  </div>
+</body>
+</html>
---
+++ TDMprojesi/TDMprojesi/index.php
@@ -0,0 +1,94 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+}
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header('location: login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page Game</title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        .go-back-btn {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background-color: #164e8a;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 50%;
+}
+
+    </style>
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+
+        document.addEventListener('DOMContentLoaded', (event) => {
+            var audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+            audio.volume = 0.5; // Adjust the volume (0.0 to 1.0)
+            const logoutLink = document.querySelector(".btn.logout");
+            const gamePagesLink = document.querySelector(".btn.game-pages");
+            logoutLink.addEventListener("mouseenter", () => {
+                audio.currentTime = 0;
+                audio.play();
+            });
+            gamePagesLink.addEventListener("mouseenter", () => {
+                audio.currentTime = 0;
+                audio.play();
+            });
+        });
+    </script>
+</head>
+
+<body>
+    <div class="header">
+        <h2>Home</h2>
+    </div>
+    <div class="container">
+        <div class="content">
+            <?php if (isset($_SESSION['success'])) : ?>
+                <div class="success">
+                    <h3>
+                        <?php
+                        echo $_SESSION['success'];
+                        unset($_SESSION['success']);
+                        ?>
+                    </h3>
+                </div>
+            <?php endif ?>
+            <?php if (isset($_SESSION['username'])) : ?>
+                <br>
+                <div class="navigation">
+                    <button class="go-back-btn" onclick="goBack()">Back</button>
+                    <h3>Welcome <strong><?php echo $_SESSION['username']; ?></strong><span></span></h3>
+                    <br>
+                    <a href="index.php?logout='1" style="color: red" class="btn logout">Logout</a>
+                    <br> <br>
+                    <a href="game.php" class="btn game-pages">Game Pages</a>
+                </div>
+            <?php endif ?>
+        </div>
+    </div>
+</body>
+
+</html>
